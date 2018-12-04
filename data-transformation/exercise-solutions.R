78933c774165d3a439d978972ada4b42be503096
Malcolm Mashig <mjm6jy@virginia.edu>
2018-12-04 17:32:00 -0500

    solution 1

--- data-transformation/exercise-solutions.R
+++ data-transformation/exercise-solutions.R
@@ -1,14 +1,34 @@
+install.packages("completejourney")
+install.packages("tidyverse")
+library(tidyverse)
 library(completejourney)
+
+#Data Tranformation Solutions
+
+#change transactions, household, week, and time variables and removed week
 
 transactions <- transactions %>% 
   select(
     quantity,
     sales_value, 
     retail_disc, coupon_disc, coupon_match_disc,
-    household_key, store_id, basket_id, product_id, 
-    week_no, day, trans_time
+    household_id, store_id, basket_id, product_id, 
+    week, transaction_timestamp
   )
+
+transactions
 
 # Question 1: Change the discount variables (i.e., retail_disc, coupon_disc,
 # coupon_match_disc) from negative to positive.
+
+transactions <- transactions %>% mutate( 
+  retail_disc = abs(retail_disc),
+  coupon_disc = abs(coupon_disc),
+  coupon_match_disc =
+  abs(coupon_match_disc))
+
+transactions
+
+#Done
+
 
