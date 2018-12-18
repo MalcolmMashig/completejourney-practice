989331ee4a23bda37a26cf308b306d7e9f6c5225
Malcolm Mashig <mjm6jy@virginia.edu>
2018-12-18 14:47:45 -0500

    more solutions

--- data-transformation/exercise-solutions.R
+++ data-transformation/exercise-solutions.R
@@ -58,3 +58,86 @@ transactions %>%
   n_distinct()
 
 #solved
+
+# Question 4: What proportion of baskets are over $10 in sales value? 
+
+transactions %>%
+  group_by(basket_id) %>%
+  summarize(basket_value = sum(sales_value)) %>%
+  ungroup() %>%
+  summarize(proportion_over_10 = mean(basket_value > 10))
+
+#solved
+
+#Question 5: Which store with over $10K in total `sales_value` discounts its products the most for loyal customers? 
+
+transactions %>%
+  filter(
+    is.finite(regular_price), 
+    is.finite(loyalty_price), 
+    regular_price > 0
+  ) %>%
+  mutate(
+    pct_loyalty_disc     = 1 - (loyalty_price / regular_price)
+  ) %>%
+  group_by(store_id) %>%
+  summarize(
+    total_sales_value    = sum(sales_value), 
+    avg_pct_loyalty_disc = mean(pct_loyalty_disc)
+  ) %>%
+  filter(total_sales_value > 10000) %>%
+  arrange(desc(avg_pct_loyalty_disc))
+
+#solved
+
+# Data Visualization Solutions
+
+# Question 1
+
+# Create a histogram of `quantity`. What, if anything, do you find unusual 
+# about this visualization? 
+  
+ggplot(transactions, mapping = aes(quantity)) + geom_histogram()
+
+# Solved
+
+# Question 2
+
+# Use a line graph to plot total sales value by day. What, if anything, do you # find unusual about this visualization?
+
+ggplot(transactions, mapping = aes(transaction_timestamp, sales_value)) + geom_line()
+
+# No day variable so I used transaction_timestamp
+
+# Alternate
+
+transactions %>% 
+  group_by(week) %>% 
+  summarize(total_sales_value = sum(sales_value, na.rm = TRUE)) %>%
+  ggplot() + 
+  geom_line(mapping = aes(x = week, y = total_sales_value))
+
+# used week instead of day
+
+# Solved
+
+# Question 3
+
+# Use a bar graph to compare the total sales values of national and private
+# -label brands. 
+
+products
+
+my_transaction_data <- left_join(transactions, products, by = 'product_id')
+
+my_transaction_data %>%
+  group_by(brand) %>%
+  summarize(total_sales_value = sum(sales_value)) %>%
+  ggplot() + 
+  geom_bar(
+    mapping = aes(x = brand, y = total_sales_value), 
+    stat = 'identity'
+  )
+
+# Solved
+
